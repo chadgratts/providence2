0e6f6cba6c960b6b93df72db6fc2abdf4d01234c
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-01-12 15:27:37 -0500

    update: changed docker-compose.yml
    default values for environment variables were moved from docker-compose.yml to .env.example.

--- backend/src/services/psqlService.ts
+++ backend/src/services/psqlService.ts
@@ -1,12 +1,12 @@
 import { Pool } from 'pg';
-// import config from '../config/environment';
+import config from '../config/environment';
 
 export class PsqlService {
   getSession = async (id: string) => {
-    return id
+    return `i went to a database and got your new salary: ${id}`
   }
 
   async addSession(id: string, data: any) {
     return [id, data]
   }
-}
+}r
