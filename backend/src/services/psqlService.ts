6ddbd1a8d15af98633a8cf88dff2f7a67bad8a1e
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-01-14 18:32:13 -0500

    build: postgres connected

--- backend/src/services/psqlService.ts
+++ backend/src/services/psqlService.ts
@@ -1,12 +1,38 @@
 import { Pool } from 'pg';
-import config from '../config/environment';
 
 export class PsqlService {
+  private pool: Pool;
+  
+  constructor() {
+    this.pool = new Pool({
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+    });
+  }
+  
   getSession = async (id: string) => {
-    return `i went to a database and got your new salary: ${id}`
+    try {
+      const result = await this.pool.query('SELECT * FROM sessions WHERE session_id = $1', [id]);
+      return result.rows;
+    } catch (error) {
+      console.error(`Error fetching session ${id} from PSQL`, error);
+      throw error;
+    }
   }
 
-  async addSession(id: string, data: any) {
-    return [id, data]
+  addSession = async (id: number, data: string) => {
+    try {
+      const result = await this.pool.query(
+        'INSERT INTO sessions (session_id, recording_data) VALUES ($1, $2) RETURNING *',
+          [id, data]
+      );
+      return result.rows[0];
+    } catch (error) {
+      console.error(`Error adding session ${id} to PSQL`, error);
+      throw error;
+    }
   }
-}r
+}
