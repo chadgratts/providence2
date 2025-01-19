772fb11997acca96c89d3a980225b3e17cdd8aac
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-01-19 14:19:44 -0500

    Added methods to redisService

--- backend/src/controllers/redisController.ts
+++ backend/src/controllers/redisController.ts
@@ -14,7 +14,7 @@ export class RedisController {
   addSession = async (req: Request, res: Response): Promise<void> => {
     const { key, data } = req.body;
     try {
-      await this.redisService.setSession(key, data);
+      await this.redisService.addSession(key, data);
       res.status(200).json({ message: `Your data ${data} was stored :)` });
     } catch (error) {
       res.status(500).json({ error: 'Failed to save data' });
@@ -26,8 +26,8 @@ export class RedisController {
     try {
       const data = await this.redisService.getSession(id); // "8 spruce"
       if (data) {
-        const summary = await this.openAIService.summarizeSession(data);
-        res.send(summary);
+        // const summary = await this.openAIService.summarizeSession(data);
+        res.send(data);
       } else {
         res.status(404).json({ error: 'Recording not found' });
       }
--- backend/src/services/redisService.ts
+++ backend/src/services/redisService.ts
@@ -4,21 +4,8 @@ export class RedisService {
   private redis: Redis;
 
   constructor() {
-    const redisUrl = process.env.REDIS_URL;
-    if (!redisUrl) {
-      throw new Error('REDIS_URL is not defined');
-    }
-    this.redis = new Redis(redisUrl);
-  }
-
-  setSession = async (key: string, data: string): Promise<void> => {
-    try {
-      console.log('Setting session in Redis:', key, data);
-      await this.redis.call('JSON.SET', key, '.', JSON.stringify(data));
-      console.log(`Session set for key: ${key}`);
-    } catch (error) {
-      console.error('Error setting session in Redis:', error);
-    }
+    if (!process.env.REDIS_URL) throw new Error('REDIS_URL is not defined');
+    this.redis = new Redis(process.env.REDIS_URL);
   }
 
   getSession = async (key: string): Promise<string | null> => {
@@ -27,6 +14,45 @@ export class RedisService {
       return data as string | null;
     } catch (error) {
       console.error('Redis session data fetch error:', error);
+      throw error;
+    }
+  }
+
+  addSession = async (key: string, data: string): Promise<void> => {
+    const sessionExists = await this.sessionExists(key);
+    if (sessionExists) {
+      await this.appendRRwebData(key, data);
+    } else {
+      await this.createNewSession(key, data)
+    }
+  }
+
+  private sessionExists = async (key: string): Promise<boolean> => {
+    try {
+      const data = await this.redis.call('JSON.GET', key);
+      return Boolean(data);
+    } catch (error) {
+      console.error('Error checking if session exists in redis', error);
+      throw error;
+    }
+  }
+
+  private createNewSession = async (key: string, value: string): Promise<void> => {
+    try {
+      await this.redis.call('JSON.SET', key, '.', value);
+      console.log(`New session created: ${key}`);
+    } catch (error) {
+      console.error(`Error creating new session for ${key} in redis`, error);
+      throw error;
+    }
+  }
+
+  private appendRRwebData = async (key: string, value: string): Promise<void> => {
+    try {
+      await this.redis.call('JSON.ARRAPPEND', key, '.events', value);
+      console.log(`#{key} addition events added to redis successfully`);
+    } catch (error) {
+      console.error(`Error appending events for ${key} in redis`, error);
       throw error;
     }
   }
