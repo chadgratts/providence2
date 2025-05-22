cc04f13c506ba4e97ef93fdd9a402beeba491210
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-05-21 20:44:46 -0500

    feat: finish routes/record route

--- backend/src/routes/record.ts
+++ backend/src/routes/record.ts
@@ -6,18 +6,36 @@ const router = express.Router();
 const redis = new RedisService();
 const psql = new PsqlService();
 
-router.post('/record', (req, res) => {
+router.post('/record', async (req, res) => {
   const { projectID, sessionID, timestamp, events } = req.body;
+  const serverTimestamp = new Date().toISOString(); // UTC
+  
+  try {
+    // Check for valid project in PSQL
+    const projectMetadata = await psql.getProject(projectID);
+    
+    if (!projectMetadata) {
+      return res.status(400).json({ error: 'Invalid project' });
+    }
 
-  // if session doesn't exist (not in progress)
-  if (!redis.sessionExists(sessionID)) {
-    psql.addSession(projectID, sessionID, timestamp);
-  } 
+    // Check for session metadata in PSQL
+    const sessionMetadata = await psql.getSession(sessionID);
 
-  redis.addRecording(sessionID, events)
-  // set expiry -- 
+    if (!sessionMetadata) {
+      await psql.addSession(sessionID, projectID, serverTimestamp);
+    } else {
+      await psql.updateSessionActivity(sessionID, serverTimestamp);
+    }
 
-})
+    // Add session event data to Redis
+    await redis.addRecording(sessionID, JSON.stringify(events));
+
+    res.status(200).json({ message: 'Events batch processed successfully' });
+  } catch (error) {
+    console.error('Error processing batch:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 
 // app.ts --> routes -->  controllers --> services
 
