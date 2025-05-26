f851ccf6121fc41b3f8ee80d4474f55ca16abbae
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-05-25 22:14:14 -0500

    chore(cleanup): remove commented SQL schema from record route

--- backend/src/routes/record.ts
+++ backend/src/routes/record.ts
@@ -39,29 +39,3 @@ router.post('/', async (req: Request, res: Response): Promise<void>=> {
 });
 
 export default router
-
-//
-//
-// CREATE TABLE sessions (
-//   id SERIAL PRIMARY KEY,
-//   project_id INTEGER NOT NULL,
-//   session_id VARCHAR(255),
-//   events_file_name VARCHAR (255) NOT NULL,
-//   session_summary VARCHAR (5000),                           
-//   session_start TIMESTAMP NOT NULL,
-//   session_end TIMESTAMP,
-//   CONSTRAINT fk_project FOREIGN KEY (project_id) REFERENCES projects(id)
-// );
-//  CREATE TABLE projects (
-//    id SERIAL PRIMARY KEY, 
-//    name VARCHAR(255) NOT NULL,
-//    password_hash VARCHAR(255)
-// );
-
-// CREATE DATABASE heimdall 
-
-// id                                        |              project_id              |                    file_name                    | session_summary |       session_start        |        session_end         |      last_activity_at      | is_active 
-// --------------------------------------+--------------------------------------+-------------------------------------------------+-----------------+----------------------------+----------------------------+----------------------------+-----------
-// 1 // 217e3db2-029a-49b6-a69c-e242aa48f401 | cfc15e83-970b-42cd-989f-b87b785a1fd4 | 217e3db2-029a-49b6-a69c-e242aa48f401-events.txt |                 | 2024-10-21 03:30:36.981+00 | 2024-10-21 03:51:59.395+00 | 2024-10-21 04:02:16.952+00 | t
-// 2 // bf71169b-1ab4-47db-903c-fdb72698e4a0 | cfc15e83-970b-42cd-989f-b87b785a1fd4 | bf71169b-1ab4-47db-903c-fdb72698e4a0-events.txt |                 | 2024-10-21 04:02:36.167+00 | 2024-10-21 04:13:15.529+00 | 2024-10-21 04:26:11.179+00 | t
-// 3 // bf71169b-1ab4-47db-903c-fdb72698e4a0 | cfc15e83-970b-42cd-989f-b87b785a1fd4 | bf71169b-1ab4-47db-903c-fdb72698e4a0-events.txt |                 | 2024-10-21 04:02:36.167+00 | 2024-10-21 04:13:15.529+00 | 2024-10-21 04:26:11.179+00 | t
