5b6cc103424572152cabb938eacd0a05891fb10c
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-05-29 23:57:39 -0500

    fix: remove nested data variable in projects route

--- backend/src/routes/projects.ts
+++ backend/src/routes/projects.ts
@@ -11,7 +11,7 @@ router.get('/:projectID', async (req, res) => {
 
   try {
     const data = await psql.getCompletedSessions(projectID);
-    res.status(200).json({ data });
+    res.status(200).json(data);
   } catch (error) {
     console.error('Error fetching completed sessions from PSQL', error);
     res.status(500).json({ error: 'Internal server error' });
