e0c696109ab8ca11fea34bc441f0d4a652204c37
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-06-23 13:17:36 -0400

    feat: update header component

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -3,25 +3,33 @@ import providenceImg from '../../assets/providence-.png'
 
 interface HeaderProps {
   onLogin?: (e:any) => void
+  project?: string
 }
 
-function Header({ onLogin }: HeaderProps) {
+function Header({ onLogin, project }: HeaderProps) {
   return (
     <header className={styles.header}>
       <svg width="280" height="60" xmlns="http://www.w3.org/2000/svg">
-        <image
-          href={providenceImg}
-          x="-10"
-          y="-17"
-          height="100px"
-          width="100px"
-        />
-        <text fill="white" x="90" y="40" fontSize="30">Providence</text>
+        <defs>
+          <linearGradient id="gradient" x1="0" y1="0" x2="1" y2="0">
+            <stop offset="0%" stopColor="#f45c5c" />
+            <stop offset="25%" stopColor="#ff8551" />
+            <stop offset="50%" stopColor="#ffaf50" />
+            <stop offset="75%" stopColor="#ffd85f" />
+            <stop offset="100%" stopColor="#f5ff80" />
+          </linearGradient>
+        </defs>
+        <image href={providenceImg} x="-10" y="-17" height="100px" width="100px"/>
+        <text fill="url(#gradient)" x="90" y="40" fontSize="30">Providence</text>
       </svg>
 
-      <button onClick={onLogin} className={styles.login}>
+      {project && <div className={`${styles.projectName} ${styles.gradientText}`}>
+        Logged in as project: {project}
+      </div>}
+
+      {!project && <button onClick={onLogin} className={styles.login}>
         Login
-      </button>
+      </button>}
     </header>
   );
 }
