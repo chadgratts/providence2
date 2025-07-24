56710493030562acf9534116347f53c37c0c7fa8
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-07-24 16:11:44 -0400

    feat: add register modal component

--- frontend/src/components/LandingPage/LandingPage.tsx
+++ frontend/src/components/LandingPage/LandingPage.tsx
@@ -3,24 +3,35 @@ import LandingHeader from '../LandingHeader'
 import Login from '../Login/Login';
 import Footer from '../Footer';
 import LoginOverlay from '../LoginOverlay'
+import RegisterOverlay from '../RegisterOverlay';
 import styles from './LandingPage.module.css'
 
 function LandingPage() {
-  const [showModal, setShowModal] = React.useState(false)
+  const [showLoginModal, setShowLoginModal] = React.useState(false)
+  const [showRegisterModal, setShowRegisterModal] = React.useState(false)
 
-  const toggleModal = function(e: any) {
-    if (showModal && ['backdrop', 'xButton'].includes(e.target.id)) {
-      setShowModal(!showModal)
-    } else if (!showModal) {
-      setShowModal(true)
+  const toggleLoginModal = function(e: any) {
+    if (showLoginModal && ['backdrop', 'xButton'].includes(e.target.id)) {
+      setShowLoginModal(!showLoginModal)
+    } else if (!showLoginModal) {
+      setShowLoginModal(true)
+    }
+  }
+
+  const toggleRegisterModal = function(e: any) {
+    if (showRegisterModal && ['backdrop', 'xButton'].includes(e.target.id)) {
+      setShowRegisterModal(!showRegisterModal)
+    } else if (!showRegisterModal) {
+      setShowRegisterModal(true)
     }
   }
 
   return (
     <div className={styles.landingPageWrapper}>
-      {showModal && <LoginOverlay onClose={toggleModal}/>}
-      <LandingHeader onLogin={toggleModal}/>
-      <Login showModal={showModal} onToggleModal={toggleModal}/>
+      {showLoginModal && <LoginOverlay onClose={toggleLoginModal}/>}
+      {showRegisterModal && <RegisterOverlay onClose={toggleRegisterModal}/>}
+      <LandingHeader onLogin={toggleLoginModal}/>
+      <Login onToggleRegisterModal={toggleRegisterModal} onToggleLoginModal={toggleLoginModal}/>
       <Footer />
     </div>
   )
