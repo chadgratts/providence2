1d211ff5c0b0eb506059a4c21bc0fdf81ac41ab6
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-07-27 20:51:06 -0400

    feat(dashboard): update login overlay component

--- frontend/src/components/LoginOverlay/LoginOverlay.tsx
+++ frontend/src/components/LoginOverlay/LoginOverlay.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './LoginOverlay.module.css';
 import xButton from '../../assets/x.png';
+import BadLoginAlert from '../BadLoginAlert';
 
 interface LoginOverlayProps {
   onClose: (e: any) => void;
 }
 
 function LoginOverlay( { onClose }: LoginOverlayProps) {
-
   const [projectName, setProjectName] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [showBadLoginAlert, setShowBadLoginAlert] = React.useState<boolean>(false)
 
   const handleNameInput = function(e: any) {
     setProjectName(e.target.value)
@@ -19,38 +20,38 @@ function LoginOverlay( { onClose }: LoginOverlayProps) {
     setPassword(e.target.value)
   }
 
+  const handleLogin = function(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    // replace to when password is wrong
+    if (true) {
+      setShowBadLoginAlert(true)
+    }
+  }
+
   return (
     <div className={styles.overlayContainer}>
       <div id='backdrop' onClick={onClose} className={styles.overlay}>
+        {showBadLoginAlert && <BadLoginAlert />}
         <div className={styles.overlayBox}>
           <div className={styles.gradientBox}>
             <p>Log in:</p>
           </div>
           <div className={styles.loginBoxContainer}>
             <div className={styles.loginBox}>
-              <form>
+              <form onSubmit={handleLogin}>
                 <div className={styles.userNameContainer}>
                   <label style={{ display: 'block'}} htmlFor='user_name'>Please enter the project user name:</label>
                   <input value={projectName} onChange={handleNameInput} id='user_name' type='text' placeholder='username'></input>
                   <label style={{ display: 'block'}} htmlFor='password'>Please enter the project password:</label>
                   <input value={password} onChange={handlePasswordInput} id='password' type='password' placeholder='password'></input>
-                  <button type='submit'>Mamma Mia Letsa Go!</button>
+                  <button type='submit'>Submit</button>
                 </div>
               </form>
             </div>
           </div>
           <div>
-            <svg className={styles.xButton} width='40' height='40' xmlns='xmlns="http://www.w3.org/2000/svg"'>
-              <image
-                id='xButton'
-                href={xButton}
-                width='30'
-                height='30'
-                x='5'
-                y='5'
-                onClick={onClose}
-              >
-              </image>
+            <svg className={styles.xButton} width='40' height='40' xmlns='http://www.w3.org/2000/svg'>
+              <image id='xButton' href={xButton} width='30' height='30' x='5' y='5' onClick={onClose} />
             </svg>
           </div>
         </div>
