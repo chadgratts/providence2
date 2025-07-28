e418d54d6b1c17e90cba57714773c61d7bbbe74e
chadgratts <89122325+chadgratts@users.noreply.github.com>
2025-07-27 21:26:57 -0400

    feat(dashboard): update SortDropdown component

--- frontend/src/components/SortDropdown/SortDropdown.tsx
+++ frontend/src/components/SortDropdown/SortDropdown.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import styles from './SortDropdown.module.css'
 
-function SortDropdown() {
- const [dropdownIsOpen, setShowSortDropdown] = React.useState(true)
-
- React.useEffect(() => {
-
- }, [])
-
-
- return (
- <div id='sortDropdown' className={styles.dropdownContainer}>
- <ul>
- <li>Sort Ascending ↑</li>
- <li>Sort Descending ↓</li>
- </ul>
- </div>
- );
+interface SortDropdownProp {
+  onClosingClick: (e: any) => void
 }
+
+const SortDropdown = React.forwardRef<HTMLDivElement, SortDropdownProp>(( {onClosingClick}, ref) => {
+  React.useEffect(() => {
+    document.addEventListener('click', onClosingClick);
+
+    return (() => {
+      document.removeEventListener('click', onClosingClick)
+    })
+  }, [])
+
+  return (
+    <div ref={ref} className={styles.dropdownContainer}>
+      <ul>
+        <li>Date Ascending ↑</li>
+        <li>Data Descending ↓</li>
+        <li>Sentiment Ascending ↑</li>
+        <li>Sentiment Descending ↓</li>
+      </ul>
+    </div>
+  );
+})
 
 export default SortDropdown;
